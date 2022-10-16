c6014fa6c67501dd9e25d46f8a09e8d64d662a09
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:12:45 +0800

    加入專案檔案。

---
+++ [作業(Exec_DeclareString 第一題)]-21謝孟勳/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_DeclareString_第一題___21謝孟勳
+{
+    //C:\temp\a.jpg
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C:\\temp\\a.jpg");
+        }
+    }
+}
